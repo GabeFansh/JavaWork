1f34065334873f05a8b039d381bdbae04eff95bf
GabeFansh <94994116+GabeFansh@users.noreply.github.com>
2022-10-12 21:11:36 -0400

    Assignment2 Final Submission
    Signed-off-by: GabeFansh <94994116+GabeFansh@users.noreply.github.com>

--- DataStructures/src/Assignment2/mtf.java
+++ DataStructures/src/Assignment2/mtf.java
@@ -36,51 +36,58 @@ public static void main(String[] args){
  *  same methods if they were implemented with LinkedList instead. (No
  *  need to explain here, only the big-O expression.)
  *
- *               |Linked List | Array List|
- *           push|           |            |
- *            pop|           |            |
- *       contains|           |            |
- *        isEmpty|           |            |
- *       toString|           |            |
+ *               | Linked List | Array List |
+ *           push|    O(n)     |    O(n)    |
+ *            pop|    O(1)     |    O(n)    |
+ *       contains|    O(n)     |    O(n)    |
+ *        isEmpty|    O(1)     |    O(1)    |
+ *       toString|    O(n)     |    O(n)    |
 *
  *
  *
- * 4. Explain each of the eight running times in the table applying the
+ * 4. Explain each of the running times in the table applying the
  * rules of counting seen in class, and for the method calls, if we have
  * covered them already, directly refer to the running times of those methods.
  *
+ *  Push for mtf is O(n) as it has to go through the whole stack and the add.
+ *  Push for arrayList is o(n) because it has to go through the whole array and remove first occurence
+ *  and then add number to the top.
+ *
+ *  Pop is O(1) as it only removes the top item in the stack
+ *  Pop is O(n) as worst case it has to go through each index to reach the last filled and remove it
+ *
+ *  Contains is O(n) for both linked list and array list as it goes through the entire stack to check for contains
+ *
+ *  isEmpty is O(1) for both linked list and array list as it only has to check for one index to see if empty
+ *
+ *  toString is O(n) for linked list as it has to iterate through every item to add to a string and print.
+ *  toString is O(n) as it takes every index data and adds to a string and prints it out.
  *
  *
 *
 *************************************************************************/
-
 import java.util.*;
 public class mtf<T>{
     // left of the list is top of the mtf
     private LinkedList<T> myData = new LinkedList<T>();
     // push removing previous occurrences of item
-    // O(n) because remove(item) is O(n) and add() is O(1) in LL
     public void push(T item){
         myData.remove(item);
         myData.add(item);
     }
     // pop
-    // O(1) because remove() is O(1) in LL
     public T pop(){
         return myData.remove();
     }
     // contains
-    // O(n) because contains() is O(n) in LL
     public boolean contains(T item){
         return myData.contains(item);
     }
     // isEmpty
-    // O(1) because isEmpty() is O(1) in LL
     public boolean isEmpty(){
         return myData.isEmpty();
     }
     // toString
-    // O(n) because toString is O(n) in LL
     public String toString(){
         return myData.toString();
     }
