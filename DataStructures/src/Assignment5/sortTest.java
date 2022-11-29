5e4fb1b3d7d1f7a4a45e25043be4f1ab6a0e5f2c
GabeFansh <94994116+gabefansh@users.noreply.github.com>
2022-11-29 15:56:56 -0500

    Assignment 6 Blank
    Signed-off-by: GabeFansh <94994116+GabeFansh@users.noreply.github.com>

--- DataStructures/src/Assignment5/sortTest.java
+++ DataStructures/src/Assignment5/sortTest.java
@@ -5,66 +5,64 @@ import java.util.Random;
 
 /**
  * Pace University
- *  Fall 2022
- *  Data Structures and Algorithms
- *
- *  Course: CS 241
- *  Team authors: Gabriel Fanshteyn
- *  External Collaborators: None
- *  References: None
- *
- *  Assignment: Assignment 4
- *  Problem: QuickSort and InsertionSort
- *  Description: Test the run time of different variations of sorting and compare them.
- *
- *
- *
- *
- *  Input: n = 10000
- *  Output:
- *
- *  Visible data fields:
- *  itemCount
- *  array
- *  insertionStart
- *  insertionEnd
- *  insertionDelta
- *  quick1Start
- *  quick1End
- *  quick1Delta
- *  quick2Start
- *  quick2End
- *  quick2Delta
- *
- *
- *  Visible methods:
- *  insertionSort()
- *  quickSort()
- *  randomQuickSort()
- *  partition()
- *  randomPartition()
- *
- *                    | increasing order | decreasing order | random   |
- *    insertionSort   |   698900         |   36340600       | 30682500 |
- *    quickSort       |   42349000       |   35034900       | 64521100 |
- *    randomQuickSort |   9773400        |  10310700        | 12152300 |
- *
- *
- *
+ * Fall 2022
+ * Data Structures and Algorithms
+ * <p>
+ * Course: CS 241
+ * Team authors: Gabriel Fanshteyn
+ * External Collaborators: None
+ * References: None
+ * <p>
+ * Assignment: Assignment 4
+ * Problem: QuickSort and InsertionSort
+ * Description: Test the run time of different variations of sorting and compare them.
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * Input: n = 10000
+ * Output:
+ * <p>
+ * Visible data fields:
+ * itemCount
+ * array
+ * insertionStart
+ * insertionEnd
+ * insertionDelta
+ * quick1Start
+ * quick1End
+ * quick1Delta
+ * quick2Start
+ * quick2End
+ * quick2Delta
+ * <p>
+ * <p>
+ * Visible methods:
+ * insertionSort()
+ * quickSort()
+ * randomQuickSort()
+ * partition()
+ * randomPartition()
+ * <p>
+ * | increasing order | decreasing order | random   |
+ * insertionSort   |   698900         |   36340600       | 30682500 |
+ * quickSort       |   42349000       |   35034900       | 64521100 |
+ * randomQuickSort |   9773400        |  10310700        | 12152300 |
+ * <p>
+ * <p>
+ * <p>
  * 4. Draw conclusions from the values observed. Are the measurements significantly different for some cases?
- *    Why? How the pivot choice helps for those cases where the running time is smaller?
- *
- *    One noticeable thing is how when the array is listed with numbers in increasing order, normal quick sort takes
- *    the longest. I believe the reason for that is because the partiton ends up for every value in the array since it's checking
- *    for a number that is less than the current one.
- *
- *    I was surprised to see that the random pivot quick sort was faster than the normal quick sort, especially when the numbers
- *    in the array are listed at random. I believe that the reason for this is the possibility that the sub array that was made
- *    from the pivot point could be mostly sorted already, which because it is random, could be a possibility. With decreasing order,
- *    insertion sort and normal quick sort were relatively the same because the process of creating a new pivot for the quick sort
- *    seems like its similar to insertion when the numbers are already in order decreasing.
- *
- *
+ * Why? How the pivot choice helps for those cases where the running time is smaller?
+ * <p>
+ * One noticeable thing is how when the array is listed with numbers in increasing order, normal quick sort takes
+ * the longest. I believe the reason for that is because the partiton ends up for every value in the array since it's checking
+ * for a number that is less than the current one.
+ * <p>
+ * I was surprised to see that the random pivot quick sort was faster than the normal quick sort, especially when the numbers
+ * in the array are listed at random. I believe that the reason for this is the possibility that the sub array that was made
+ * from the pivot point could be mostly sorted already, which because it is random, could be a possibility. With decreasing order,
+ * insertion sort and normal quick sort were relatively the same because the process of creating a new pivot for the quick sort
+ * seems like its similar to insertion when the numbers are already in order decreasing.
  */
 
 public class sortTest {
@@ -74,7 +72,7 @@ public class sortTest {
 
         int[] array = new int[itemCount];
 
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             array[i] = rand1.nextInt(itemCount);
         }
 
@@ -101,47 +99,44 @@ public class sortTest {
                 "Random Quick Sort: " + quick2Delta);
 
 
-
-
     }
 
 
-
-    public static void insertionSort(int[] a){
-        for (int j=1; j < a.length; j++){
+    public static void insertionSort(int[] a) {
+        for (int j = 1; j < a.length; j++) {
             int key = a[j];
             int i = j - 1;
-            while (i > 0 && a[i] > key){
-                a[i+1] = a[i];
-                i = i-1;
+            while (i > 0 && a[i] > key) {
+                a[i + 1] = a[i];
+                i = i - 1;
             }
-            a[i+1] = key;
+            a[i + 1] = key;
         }
 
 
     }
 
-    public static void quickSort(int[] a, int p, int r){
-        if (p < r){
+    public static void quickSort(int[] a, int p, int r) {
+        if (p < r) {
             int q = partition(a, p, r);
-            quickSort(a, p, q-1);
-            quickSort(a, q+1, r);
+            quickSort(a, p, q - 1);
+            quickSort(a, q + 1, r);
         }
     }
 
-    public static void randomQuickSort(int[] a, int p, int r){
-        if (p < r){
+    public static void randomQuickSort(int[] a, int p, int r) {
+        if (p < r) {
             int q = randomPartition(a, p, r);
-            randomQuickSort(a, p, q-1);
-            randomQuickSort(a, q+1, r);
+            randomQuickSort(a, p, q - 1);
+            randomQuickSort(a, q + 1, r);
         }
     }
 
 
-    public static int partition(int[] a, int p, int r){
+    public static int partition(int[] a, int p, int r) {
         int x = a[r];
         int i = p - 1;
-        for (int j = p;j < r; j++) {
+        for (int j = p; j < r; j++) {
             if (a[j] <= x) {
                 i++;
 
@@ -151,13 +146,13 @@ public class sortTest {
 
             }
         }
-        int temp = a[i+1];
-        a[i+1] = a[r];
+        int temp = a[i + 1];
+        a[i + 1] = a[r];
         a[r] = temp;
-        return i+1;
+        return i + 1;
     }
 
-    public static int randomPartition(int[] a, int p, int r){
+    public static int randomPartition(int[] a, int p, int r) {
 
         Random rand = new Random();
         int piv = rand.nextInt(r - p) + p;
@@ -168,7 +163,7 @@ public class sortTest {
 
         int x = a[r];
         int i = p - 1;
-        for (int j = p;j < r; j++) {
+        for (int j = p; j < r; j++) {
             if (a[j] <= x) {
                 i++;
 
@@ -178,15 +173,11 @@ public class sortTest {
 
             }
         }
-        int temp = a[i+1];
-        a[i+1] = a[r];
+        int temp = a[i + 1];
+        a[i + 1] = a[r];
         a[r] = temp;
-        return i+1;
+        return i + 1;
     }
-
-
-
-
 
 
 }
